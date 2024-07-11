a53ec84d322bf81c2df94d6a4416e1319caa74da
kishan <guptakishan492@gmail.com>
2024-07-11 18:05:49 +0530

    bug1

--- src/test/java/com/example/cabbooking/CabBookingBackendApplicationTests.java
+++ src/test/java/com/example/cabbooking/CabBookingBackendApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.cabbooking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.create.CabBookingBackendApplication;
 
 @SpringBootTest
+@ContextConfiguration(classes = CabBookingBackendApplication.class)
 class CabBookingBackendApplicationTests {
 
 	@Test
